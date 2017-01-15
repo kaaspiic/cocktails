14892ecc4fd23df74e5d27f799bc611ecf44c0ed
kaaspiic <kaasiis2@inbox.lv>
2017-01-15 23:12:46 +0200

    some cool hacker shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable
 
   validates :email, presence: true
-	has_many :drinks
+  has_many :drinks
   def favorites
     Drink.where(id: super)
   end
